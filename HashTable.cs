0be78d06c06b962ce37bea27be617de12129261a
NicoDVK <71187149+NicoDVKa@users.noreply.github.com>
2020-10-20 00:26:08 -0300

    Comienzo
    Dispersion abierta, ejercicio 1.

---
+++ HashTable.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP4
+{
+    class HashTable
+    {
+        private int tamaño;
+        private ArrayList[] tablaHA;//Dispersion abierta
+        private int[] tablaHC;//Dispersion cerrada
+
+        public HashTable(int tamaño)
+        {
+            this.tamaño = tamaño;
+            this.tablaHA = new ArrayList[tamaño]; 
+            this.tablaHC= new int[tamaño];
+        }
+
+        //Dispersion abierta
+
+        public void insertarHA(int num)
+        {
+            int clave = num % tamaño; //Funcion de hash
+
+            if (tablaHA[clave]==null)
+            {
+                tablaHA[clave] = new ArrayList();
+                tablaHA[clave].Add(num);
+            }
+            else
+            {
+                tablaHA[clave].Add(num);
+            }
+        }
+
+        public void verHA()
+        {
+            for (int i=0; i<tamaño;i++)
+            {
+                Console.WriteLine("En la posicion {0}:",i);
+                if (this.tablaHA[i]!=null)
+                {
+                    foreach (int x in this.tablaHA[i])
+                    {
+                        Console.WriteLine(x);
+                    }
+                }
+            }
+        }
+
+
+        //Dispersion cerrada
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TP4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashTable tabla1 = new HashTable(11);
+
+            tabla1.insertarHA(5);
+            tabla1.insertarHA(20);
+            tabla1.insertarHA(3);
+            tabla1.insertarHA(1000);
+            tabla1.insertarHA(45);
+            tabla1.insertarHA(27);
+            tabla1.insertarHA(25);
+
+            tabla1.verHA();
+
+
+
+
+            Console.ReadKey(true);
+
+        }
+
+        static public int hash(int x) //Funcion de hash especifica para este problema
+        {
+            return x % 11;
+        }
+    }
+}
